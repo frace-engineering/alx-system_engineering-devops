16f726868343ff3ea22aed700a6c37da42a54830
Ugwu Friday B <fugwungene@gmail.com>
2023-11-24 22:16:30 +0000

    Use right space tab

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,9 @@
 # Creat a file using puppet in the /tmp/school path
- file {'create file':
-   ensure  => 'present',
-   content => 'I love Puppet',
-   mode    => '0744',
-   owner   => 'www-data',
-   group   => 'www-data',
-   path    => '/tmp/school',
- }
+file {'create file':
+  ensure  => 'present',
+  content => 'I love Puppet',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  path    => '/tmp/school',
+}
