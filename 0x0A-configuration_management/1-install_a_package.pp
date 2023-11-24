6b5467fb8c8524b5f530c73fe2328aa107b723a7
Ugwu Friday B <fugwungene@gmail.com>
2023-11-24 23:29:14 +0000

    Update code

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,11 @@
 # Install 'flask ffrom pip3 using puppet.
 
-package {'install flask==2.1.0':
+package {'flask':
   ensure   => '2.1.0',
   provider => 'pip3',
+  require  => Exec['install flask'],
+}
+
+exec { 'install flask':
+  command => '/usr/bin/pip3 install flask'
 }
