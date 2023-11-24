98bf61c8e8f427277825b9dcb7b49183dbab05b0
Ugwu Friday B <fugwungene@gmail.com>
2023-11-24 23:15:38 +0000

    Refactor code

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,6 @@
 # Install 'flask ffrom pip3 using puppet.
 
-exec { 'install flask=2.1.0':
-  path => '/usr/bin/pip3 install flask==2.1.0'
+package {'install flask==2.1.0':
+  ensure   => '2.1.0',
+  provider => 'pip3',
 }
