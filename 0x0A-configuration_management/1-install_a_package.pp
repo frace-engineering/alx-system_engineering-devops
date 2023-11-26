f6de75606b520ed95122eb3c3b17732ec48257b8
Ugwu Friday B <fugwungene@gmail.com>
2023-11-26 18:06:28 +0000

    Add advanced task

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -2,5 +2,14 @@
 
 package {'flask':
   ensure   => '2.1.0',
+  allow    => '2.1.0',
+  require  => Package['werkzeug'],
+}
+package {'werkzeug':
+  ensure   => 'latest',
   provider => 'pip3',
+} 
+exec {'install flask':
+  command => '/usr/bin/pip3',
+  require => Package['flask'],
 }
