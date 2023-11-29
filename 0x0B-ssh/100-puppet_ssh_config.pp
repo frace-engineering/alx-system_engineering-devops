74a63b8dd22e32d0cc39af6d26098dd94b6a9af6
Ugwu Friday B <fugwungene@gmail.com>
2023-11-29 22:57:55 +0000

    Restart the apache using the docker id

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,16 +1,16 @@
 # A puppet manifest that configures SSH client.
 
-file { '/home/fugwungene/.ssh/config':
+file {'/home/fugwungene/.ssh/config':
   ensure  => 'file',
-  owner   => 'fugwungene',
-  group   => 'fugwungene',
-  mode    => '0600',
-  content => '<<-EOT
+  content => ' 
     Host server1
       HostName 181408-web-01
       user ubuntu
       IdentityFile ~/.ssh/school
       PreferredAuthentications publickey
       PasswordAuthentication no
-  EOT'
+  ',
+  owner   => 'fugwungene',
+  group   => 'fugwungene',
+  mode    => '0600',
 }
