3998307b495f7ba23c5e93920cdcc7e750416517
Engr Ugwu <fugwungene@gmail.com>
2024-01-16 22:16:14 +0000

    Remove headers

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,21 +1,21 @@
 #!/usr/bin/python3
-import requests
 """ Function that queries the Reddit API.
 
 This function returns the number of subscribers
 (not active user,but total subscribers) for a given subreddit.
 """
+import requests
 
 
 def number_of_subscribers(subreddit):
     """Return total number of subscribers on the subreddit.
 
     If an invalid subreddit is given, return 0 """
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {
+    url = "https://www.reddit.com/r/{:s}/about.json".format(subreddit)
+    """headers = {
         "User-Agent": "linux:0x16.api.advanced:v1.00 (by /u/bdov_)"
-    }
-    response = requests.get(url, headers=headers, allow_redirects=False)
+    }"""
+    response = requests.get(url, allow_redirects=False)
     if response.status_code == 404:
         return 0
     result = response.json().get("data")
