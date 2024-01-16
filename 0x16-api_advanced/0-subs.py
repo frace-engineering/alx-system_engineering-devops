ce01f34d511ed478ddc9ab83501422e75305751b
Engr Ugwu <fugwungene@gmail.com>
2024-01-16 22:40:04 +0000

    Fix bug in code

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -12,10 +12,10 @@ def number_of_subscribers(subreddit):
 
     If an invalid subreddit is given, return 0 """
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    """headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.00 (by /u/bdov_)"
-    }"""
-    response = requests.get(url, allow_redirects=False)
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+    }
+    response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code != 200:
         return 0
     result = response.json().get("data")
