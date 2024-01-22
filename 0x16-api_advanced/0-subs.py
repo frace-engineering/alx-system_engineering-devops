cd20b52a4b6f83ce35266af3b9f8fa956bdde7cd
Engr Ugwu <fugwungene@gmail.com>
2024-01-22 04:03:35 +0000

    Fix bug in code

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -10,13 +10,14 @@ import requests
 def number_of_subscribers(subreddit):
     """Return total number of subscribers on the subreddit.
 
-    If an invalid subreddit is given, return 0 """
+    If an invalid subreddit is given, return 0
+    """
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {
         "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
     }
     response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code > 200:
+    if response.status_code == 404:
         return 0
-    result = response.json().get('data')
+    result = response.json().get("data")
     return result.get("subscribers")
